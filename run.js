58ce2a17001d413581d579ec945e1dd5bca40c49
pjama <phil.jama@gmail.com>
2016-05-31 23:19:33 -0700

    small edits, remove use strict

--- run.js
+++ run.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs          = require("fs");
 const TestRunner  = require("./test-runner");
 
--- submission-client.js
+++ submission-client.js
@@ -1,10 +1,7 @@
 const request = require("request");
-// const SUBMISSION_URL = "http://compass.lighthouselabs.ca/assessments/";
-const SUBMISSION_URL = "http://127.0.0.1:8080/assessments/";
+const SUBMISSION_URL = "http://127.0.0.1:8080/assessments/"; // TODO Update this submission URL
 
 class SubmissionClient {
-  constructor() { }
-  
   submit(testResultData, cb) {
     request({
       url: SUBMISSION_URL,
