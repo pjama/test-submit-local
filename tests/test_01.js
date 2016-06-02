fbf3e66a0864cfdc375bd9419416cfe2e336710f
pjama <phil.jama@gmail.com>
2016-06-02 09:18:06 -0700

    add example to spy on function and assert it has not been called

--- tests/test_01.js
+++ tests/test_01.js
@@ -8,12 +8,20 @@ const variableX = libModule.x;
 
 describe("Function", () => {
   it("Should return null when popping an empty array", () => {
-    var pp = new PushPopper();
-    
+    const pp = new PushPopper();
     assert.deepEqual(pp.push(1), [1])
   });
   
   it("Should simply equal to twelve!", () => {
     assert.equal(variableX, 12);
+  });
+  
+  it("Should not use Math.sqrt", () => {
+    // let sqrtBack = Math.sqrt;
+    let spySqrt = sinon.spy(Math, "sqrt");
+    const pp = new PushPopper();
+    pp.sqrt(4);
+    sinon.assert.notCalled(spySqrt);
+    Math.sqrt.restore();
   });
 });
